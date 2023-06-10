a6148d0e4f38594c6b98e5d0f370ead35e27f2ca
Stefan de Gier <stefan_de_gier@outlook.com>
2023-06-10 13:33:32 +0200

    Updated HomePage:
    - Added header with navigation
    - Added new navigation bar
    - Added flex-box for components

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,21 +1,77 @@
 <template>
-    <ion-page>
+  <ion-page>
+    <ion-content :fullscreen="true">
       <ion-header>
         <ion-toolbar>
-          <ion-title>VIEW</ion-title>
+          <ion-buttons slot="start">
+            <!-- Eventuele knoppen aan het begin van de navigatiebalk -->
+          </ion-buttons>
+          <ion-title>View</ion-title>
+          <ion-buttons slot="end">
+            <ion-button router-link="/settings-page" router-direction="forward">
+              <ion-icon slot="icon-only" :ios="settingsIcon" :md="settingsIcon"></ion-icon>
+            </ion-button>
+          </ion-buttons>
         </ion-toolbar>
       </ion-header>
-      <ion-content :fullscreen="true">
-        <ion-header collapse="condense">
-          <ion-toolbar>
-            <ion-title size="large">VIEW</ion-title>
-          </ion-toolbar>
-        </ion-header>
-       <alert-component />
+      <ion-content>
+        <!-- Hier de components -->
+        <div class="flex-container">
+          <div class="top-section">
+            <!-- De building moet circa 70% van het scherm hebben -->
+            <BuildingComponent />
+          </div>
+          <div class="bottom-section">
+            <!-- De stappenplan moet circa 30% van het scherm hebben (onderaan) -->
+            <EmergencyPlanComponent />
+          </div>
+        </div>
       </ion-content>
-    </ion-page>
-  </template>
+    </ion-content>
+  </ion-page>
+</template>
 
-  <script setup>
-import AlertComponent from "@/components/AlertComponent.vue"
+<script lang="ts">
+import { IonTitle, IonPage, IonButton, IonButtons, IonToolbar, IonHeader, IonIcon, IonContent } from '@ionic/vue';
+import { settings } from 'ionicons/icons';
+import BuildingComponent from "@/components/BuildingComponent.vue"
+import EmergencyPlanComponent from "@/components/EmergencyPlanComponent.vue"
+
+export default {
+  components: {
+    IonPage,
+    IonTitle,
+    IonButton,
+    IonButtons,
+    IonToolbar,
+    IonHeader,
+    IonIcon,
+    IonContent,
+    BuildingComponent,
+    EmergencyPlanComponent
+  },
+  computed: {
+    settingsIcon() {
+      return settings;
+    },
+  }
+};
 </script>
+
+<style>
+  .flex-container {
+    display: flex;
+    flex-direction: column;
+    height: 100vh; /* Volledige hoogte van het scherm */
+  }
+
+  .top-section {
+    flex: 0 0 70%; /* 70% van de flexcontainerhoogte */
+    background-color: red; /* Vervang dit met de gewenste achtergrondkleur */
+  }
+
+  .bottom-section {
+    flex: 0 0 30%; /* 30% van de flexcontainerhoogte */
+    background-color: blue; /* Vervang dit met de gewenste achtergrondkleur */
+  }
+</style>
