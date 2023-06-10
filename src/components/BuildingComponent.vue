300494a03b0ce5d61c369792fe6c2bde4c9a7598
marisnieuwen <0983425@hr.nl>
2023-06-10 14:57:44 +0200

    Add building image to assets  + building component

--- src/components/BuildingComponent.vue
+++ src/components/BuildingComponent.vue
@@ -1,13 +1,25 @@
 <template>
-	<h1>BuildingComponent</h1>
+	<!-- <h1>BuildingComponent</h1> -->
+
+	<div class="building">
+        <img src="/assets/building.svg" />
+      </div>
+
 </template>
   
 <script lang="ts">
-
-export default {
-
-};
+export default {};
 </script>
+
 <style scoped>
-    
+  .building {
+    display: flex;
+    justify-content: center;
+    height: 100%;
+    padding-top: 2.6875rem; /* 43 px padding per design */
+  }
+  .building img {
+    width: 165px;
+    height: 357px;
+  }
 </style>
